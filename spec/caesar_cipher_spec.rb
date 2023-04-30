7ab695dd1022411410fe3314e86b3bc478a8282d
Sergio Guerra <sergioguerra6188@gmail.com>
2023-04-29 21:13:27 -0400

    Test if #caesar_cipher does not change punctuation

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "returns an encrypted message based on the specified letter shift" do 
     expect(caesar_cipher("Zimbabwe", 5)).to eql("Enrgfgbj")
   end
+
+  it "does not affect punctuation" do 
+    expect(caesar_cipher("AB.CD.EF", 1)).to eql("BC.DE.FG")
+  end
 end
